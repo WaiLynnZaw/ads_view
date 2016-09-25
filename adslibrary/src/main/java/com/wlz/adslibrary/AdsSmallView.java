5e5156f38458dce2fd8ca62cc1fa9358be60aa5e
WaiLynnZaw <kolynn1500@gmail.com>
2016-09-25 19:02:03 +0630

    initial setup

---
+++ adslibrary/src/main/java/com/wlz/adslibrary/AdsSmallView.java
@@ -0,0 +1,17 @@
+package com.wlz.adslibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by WaiLynnZaw on 9/23/16.
+ */
+
+public class AdsSmallView extends View {
+    public AdsSmallView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+}
